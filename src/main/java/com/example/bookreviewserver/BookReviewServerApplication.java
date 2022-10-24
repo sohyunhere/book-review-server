3cebd181acfe5f48ea84708f88a020c6b9ec5e7e
sh_park <shparkso162@gmail.com>
2022-10-24 17:25:45 +0900

    Initial commit

---
+++ src/main/java/com/example/bookreviewserver/BookReviewServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookreviewserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookreviewserver/BookReviewServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookreviewserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
